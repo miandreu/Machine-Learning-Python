40a5bb8c2f6ad8c285b3998f1b8e582287ecb8b0
miquel <miquelangelandreu@gmail.com>
2017-12-15 20:38:27 +0100

    Initial commit

---
+++ Sección_5_Problema_Señales/Inicializador.py
@@ -0,0 +1,49 @@
+"""
+Author: @miandreu
+"""
+
+"""
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# IMPORTS
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+"""
+import Machine_Learning_test_miquel.Sección_5_Problema_Señales.Modelo_Convolucional as modelo
+import Machine_Learning_test_miquel.Sección_5_Problema_Señales.Buscador as buscador
+from Machine_Learning_test_miquel.UsefulTools.TensorFlowUtils import pt
+import tensorflow as tf
+
+"""
+Ubicaciones
+"""
+conjunto_entrenamiento = "D:\\Machine_Learning\\Competitions\\Miquel_Signal\\DITS-classification\\classification_train\\"
+conjunto_test = "D:\\Machine_Learning\\Competitions\\Miquel_Signal\\DITS-classification\\classification_test\\"
+
+numero_clases = 59 # Start in 0
+
+searcher = buscador.Buscador(path=[conjunto_entrenamiento, conjunto_test], numero_clases=numero_clases)
+searcher.encuentra_conjuntos_entrenamiento_test_desde_path()
+
+percentages_sets = None  # Example
+
+"""
+Getting train, validation (if necessary) and test set.
+"""
+test_set = [searcher.x_test, searcher.y_test]  # Test Set
+train_set = [searcher.x_train, searcher.y_train]  # Train Set
+
+pt(test_set[0])
+pt(test_set[1])
+del searcher
+option_problem = "Problema de señales"
+
+models = modelo.Modelo(input=train_set[0],test=test_set[0],
+                         input_labels=train_set[1],test_labels=test_set[1],
+                         number_of_classes=numero_clases,
+                         option_problem=option_problem)
+with tf.device('/gpu:0'):
+    models.convolucion_imagenes()
+
+
+
